205198b4645282c17d1e540339b2e9b9f5bdc5e0
Alexis Jones <shyniece21@yahoo.com>
2022-12-10 20:10:09 -0700

    Add objects answers

--- objects.js
+++ objects.js
@@ -6,26 +6,48 @@
 // file in your Terminal.
 
 foods = {
-  apples: 23, 
-  grapes: 507, 
+  apples: 23,
+  grapes: 507,
   eggs: 48
 }
 
-// Write code that prints all of the 'keys' of the foods variable 
+// Write code that prints all of the 'keys' of the foods variable
 // you created above:
-// YOUR CODE HERE
+foods = {
+  apples: 23,
+  grapes: 507,
+  eggs: 48
+};
+console.log(Object.keys(foods))
 
-// Write code that prints all of the 'values' of the foods variable 
+// Write code that prints all of the 'values' of the foods variable
 // you created above:
-// YOUR CODE HERE
+foods = {
+  apples: 23,
+  grapes: 507,
+  eggs: 48
+};
+console.log(Object.values(foods))
 
-// Write code that prints the value of the second food of the foods variable 
+// Write code that prints the value of the second food of the foods variable
 // you created above:
-// YOUR CODE HERE
+foods = {
+  apples: 23,
+  grapes: 507,
+  eggs: 48
+};
+console.log(foods.grapes)
 
-// Write code that adds a food to the foods object. 
+// Write code that adds a food to the foods object.
 // Then, print the updated object:
-// YOUR CODE HERE
+foods = {
+  apples: 23,
+  grapes: 507,
+  eggs: 48
+};
+foods.Oranges = 99
+
+console.log(foods)
 
 
 #-------------------
@@ -40,15 +62,38 @@ foods = {
 // value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
 
 // Write code that logs your email object to the terminal.
+userOne = {
+  sender: "Homer Simpson",
+  email: "hsimpson@gmail.com",
+  subject: "Urgent",
+  Date: 121022,
+  Time: "1245 pm"
+};
+console.log(userOne)
 
-
-// Write code that logs all of the 'keys' of the email object 
+// Write code that logs all of the 'keys' of the email object
 // you created above:
-// YOUR CODE HERE
 
-// Write code that logs all of the 'values' of the email object 
+userOne = {
+  sender: "Homer Simpson",
+  email: "hsimpson@gmail.com",
+  subject: "Urgent",
+  Date: 121022,
+  Time: "1245 pm"
+};
+console.log(Object.keys(userOne))
+
+// Write code that logs all of the 'values' of the email object
 // you created above:
-// YOUR CODE HERE
+
+userOne = {
+  sender: "Homer Simpson",
+  email: "hsimpson@gmail.com",
+  subject: "Urgent",
+  Date: 121022,
+  Time: "1245 pm"
+};
+console.log(Object.values(userOne))
 
 
 #-------------------
@@ -76,7 +121,7 @@ posts = [
     timestamp: "4:37 PM August 13, 2019",
     number_likes: 0,
     comments: []
-  }, 
+  },
   {
     imageSrc: "./images/holiday-party.png",
     caption: "What a great holiday party omg",
@@ -90,12 +135,37 @@ console.log(posts);
 console.log(posts[0]);
 
 
-// The code snippet above shows an Array with 2 elements. Each element in the Array is a 
-// Objects. Each of those Objects has 4 key-value pairs. This may LOOK 
+// The code snippet above shows an Array with 2 elements. Each element in the Array is a
+// Objects. Each of those Objects has 4 key-value pairs. This may LOOK
 // a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 // good to have some exposure before going into Mod 1.
 
+users = [
+  {
+  sender: "Lisa Simpson",
+  email: "lsimps0n@gmail.com",
+  subject: "Band Practice",
+  Date: 120122,
+  Time: "1245 pm"
+ },
+ {
+  sender: "Bart Simpson",
+  email: "bsimpson10@gmail.com",
+  subject: "Jokes",
+  Date: 121522,
+  Time: "1206 pm"
+  },
+  {
+  sender: "Marge Simpson",
+  email: "msimpson@gmail.com",
+  subject: "Hello",
+  Date: 121122,
+  Time: "1117 pm"
+  }
+];
+console.log(users);
 
-// YOU DO: Create an array of at least 3 EMAIL Objects, using the same 
+
+// YOU DO: Create an array of at least 3 EMAIL Objects, using the same
 // key-value pairs you used in your email Object above.
 // Then, print the email Array to the Terminal.
